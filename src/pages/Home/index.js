a2bfc60a6573bb738009bd6b2fd420428453cbd8
JulieBourelDrapier <juliebourel@hotmail.com>
2023-09-14 10:16:39 +0200

    ajout d'une condition pour vérifier si last existe avant d'afficher le composant EventCard

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -116,6 +116,7 @@ const Page = () => {
     <footer className="row">
       <div className="col presta">
         <h3>Notre derniére prestation</h3>
+        {last ? (
         <EventCard
           imageSrc={last?.cover}
           title={last?.title}
@@ -123,6 +124,9 @@ const Page = () => {
           small
           label="boom"
         />
+        ) : (
+        <p>PRESTATIONS INDISPONIBLES</p>
+        )}
       </div>
       <div className="col contact">
         <h3>Contactez-nous</h3>
