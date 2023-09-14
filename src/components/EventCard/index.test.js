daa841ac659ca03d44c59890e35096e17d6a7a33
JulieBourelDrapier <juliebourel@hotmail.com>
2023-09-14 11:56:48 +0200

    back to /avril/

--- src/components/EventCard/index.test.js
+++ src/components/EventCard/index.test.js
@@ -23,7 +23,7 @@ describe("When a event card is created", () => {
       />
     );
     const titleElement = screen.getByText(/test event/);
-    const monthElement = screen.getByText(/getMonth(Date)/);
+    const monthElement = screen.getByText(/avril/);
     const labelElement = screen.getByText(/test label/);
     expect(titleElement).toBeInTheDocument();
     expect(labelElement).toBeInTheDocument();
